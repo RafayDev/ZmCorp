cd0b6e2ef02b4d599dff931c603586aaa2ae5ccb
RafayDev <imabdulrafayy@gmail.com>
2023-12-01 19:33:52 -0800

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-gh-pages/' : '',
+}
