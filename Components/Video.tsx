e10fe472ad73fa42e65aa2cab75dfc65502e2a0c
RafayDev <imabdulrafayy@gmail.com>
2023-12-01 20:12:45 -0800

    tesing

--- Components/Video.tsx
+++ Components/Video.tsx
@@ -4,7 +4,7 @@ const Video = () => {
   return (
     <div>
         <video autoPlay loop muted className="min-w-full sm:h-3/4   lg:h-3/4    ">
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="/Video.mp4" type="video/mp4" />
       </video>
       <div  className="absolute bg-opacity-50 top-1/4 rounded-lg left-1/2 bg-black transform -translate-x-1/2 -translate-y-1/2 text-center  text-white ">
         <h1 className="text-xs sm:text-xl md:text-2xl font-bold lg:text-4xl xl:text-6xl">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
