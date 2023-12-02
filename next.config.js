32ba078905b171aa06248c684ff6d169317d42a3
RafayDev <imabdulrafayy@gmail.com>
2023-12-01 19:50:57 -0800

    testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-gh-pages/' : '',
-}
+module.exports = nextConfig
